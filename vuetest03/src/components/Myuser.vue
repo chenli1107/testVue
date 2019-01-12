fce78c6ac5057c71628674d4a1c2968ed51aade9
chenli1107 <chenli1107@163.com>
2019-01-12 23:33:03 +0800

    111

---
+++ vuetest03/src/components/Myuser.vue
@@ -0,0 +1,25 @@
+<template>
+	<div class="user">
+		<h1> 用户列表 </h1>
+		<ul>
+		  <li v-for="u in users">{{u}}</li>
+		</ul>
+	</div>
+</template>
+
+<script>
+  console.log(1111);
+  export default {
+    data(){
+      return {
+        users: ["tom", "jack", "mike"]
+      }
+    }
+  }
+</script>
+
+<style>
+  h1{
+    color: red;
+  }
+</style>
